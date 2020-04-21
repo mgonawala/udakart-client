eb524875df26b10ebb6df2ead1d487bb950e21c7
mgonawala <mohini.gonawala90@gmail.com>
2020-04-21 18:29:28 +0530

    Add NotFound component & it's Route fro 404s.

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,16 @@
 import {Switch, Route} from 'react-router-dom'
 import Home from "./components/Home";
 import React from 'react';
+import NotFound from "./components/NotFound";
 
 export default function Routes(){
   return(
     <Switch>
       <Route exact path="/">
         <Home/>
+      </Route>
+      <Route>
+        <NotFound/>
       </Route>
     </Switch>
   );

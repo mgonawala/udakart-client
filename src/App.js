2bb06eeab05c66d72dd4bca43e651630ab6c5454
mgonawala <mohini.gonawala90@gmail.com>
2020-04-21 18:23:51 +0530

    Add Links
    This adds Links to Login and Signup routes.
    It Uses LinkContainer Element which prvents browser refreshing when you click on the link.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Navbar} from "react-bootstrap";
+import {Navbar, Nav, NavItem, Form, FormControl, Button} from "react-bootstrap";
+import {LinkContainer} from 'react-router-bootstrap';
 import {Link} from 'react-router-dom';
 import Routes from "./Routes";
 import "./App.css";
@@ -13,7 +14,25 @@ export default function App(){
             <Link to={"/"}>Udakart App</Link>
           </Navbar.Brand>
           </Navbar.Header>
+
+          <Navbar.Collapse>
+
+            <Nav className={"mr-auto"} pullRight>
+              <LinkContainer to={"/login"}>
+              <NavItem >Login</NavItem>
+              </LinkContainer>
+              <LinkContainer to={"/signup"}>
+              <NavItem >Sign Up</NavItem>
+              </LinkContainer>
+            </Nav>
+            <Form inline className={"inlineform"}>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+
         </Navbar>
+
         <Routes/>
       </div>
   );

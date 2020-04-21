cb1286d4c93164aaa99ecc7775a5c923a10d4be2
mgonawala <mohini.gonawala90@gmail.com>
2020-04-21 18:51:46 +0530

    Add Login Component

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,16 @@ import {Switch, Route} from 'react-router-dom'
 import Home from "./components/Home";
 import React from 'react';
 import NotFound from "./components/NotFound";
+import Login from "./components/Login";
 
 export default function Routes(){
   return(
     <Switch>
       <Route exact path="/">
         <Home/>
+      </Route>
+      <Route exact path="/login">
+        <Login/>
       </Route>
       <Route>
         <NotFound/>
---
+++ src/components/Login.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import {FormGroup, FormControl, ControlLabel, Button} from 'react-bootstrap'
+import "./Login.css"
+
+export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm(){
+    return email.length>0 && password.length>0
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+  }
+  function onEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function onPasswordChange(event) {
+    setPassword(event.target.value)
+  }
+  return(
+
+      <div className={"Login"}>
+        <form onSubmit={handleSubmit}>
+
+
+          {/** Email Control **/}
+          <FormGroup controlId={"email"} bsSize={"large"}>
+            <ControlLabel>Email</ControlLabel>
+            <FormControl type={"email"} value={email} autoFocus onChange={onEmailChange}/>
+          </FormGroup>
+
+          {/* Password Control */}
+          <FormGroup controlId={"password"} bsSize={"large"}>
+            <ControlLabel>Email</ControlLabel>
+            <FormControl type={"password"} value={password} autoFocus onChange={onPasswordChange}/>
+          </FormGroup>
+
+          <Button block type={"submit"} bsSize={"large"} disabled={!validateForm()} >
+            Login
+          </Button>
+        </form>
+      </div>
+  );
+}
